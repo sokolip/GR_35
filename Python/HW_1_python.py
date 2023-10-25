e7f32b35b5bbe6bd4c088ff707917b4f0b0ea176
sokolip <sokolip8@gmail.com>
2023-10-25 13:14:17 -0500

    HW_2

---
+++ Python/HW_1_python.py
@@ -0,0 +1,22 @@
+n1 = "Vadim"
+n2 = 10
+n3 = 3.14
+n4 = False
+n4_1 =True
+n5 = [1,2, '3443', True, [33,'Fly']]
+n6 = (1, 2, 3,4,)
+n7 = {'Name':'Vadim',
+      'age' : 34,
+      3: 'age'}
+
+print(type(n1), (n1))
+print(type(n2), (n2))
+print(type(n3), (n3))
+print(type(n4), (n4))
+print(type(n5), (n5))
+print(type(n6), (n6))
+print(type(n7), (n7))
+
+
+
+
